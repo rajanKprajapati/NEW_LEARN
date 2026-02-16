37845866a8696c36b3b5ddc23c54ae2371c8cb08
Rajan Kumar Prajapati <toptener188@gmail.com>
2026-02-16 22:30:37 +0530

    THis is the program which clear the image of Abstraction concept pro_2.py

---
+++ Abstraction/pro_2.py
@@ -0,0 +1,59 @@
+"""
+Q2: Employee Payroll System
+
+Abstract class Employee → abstract methods calculate_salary(), get_details()
+
+Implement FullTimeEmployee and PartTimeEmployee
+
+Print total payroll"""
+
+from abc import ABC,abstractmethod
+class Employee(ABC):
+    @abstractmethod
+    def calculate_salary(self):
+        pass
+    @abstractmethod
+    def get_details(self):
+        pass
+
+
+class FullTimeEmployee(Employee):
+    def __init__(self,Tsalary,bonus,pf,tex):
+        self.Tsalary=Tsalary
+        self.bonus=bonus
+        self.pf=pf
+        self.tex=tex
+
+    def calculate_salary(self):
+        total_earning=self.Tsalary+self.bonus
+        total_reduction=self.pf+self.tex
+        current_amount=total_earning-total_reduction
+        return current_amount
+    
+    def get_details(self):
+        print(f"Your Salary:{self.Tsalary},Bonus:{self.bonus},Pf:{self.pf},TEX:{self.tex}")
+class PartTimeEmployee(Employee):
+    def __init__(self,Tsalary,bonus):
+        self.Tsalary=Tsalary
+        self.bonus=bonus
+    
+    def calculate_salary(self):
+        total_earning=self.Tsalary+self.bonus
+        return total_earning
+
+    def get_details(self):
+        print(f"Your Salary:{self.Tsalary},Bonus:{self.bonus}")
+
+    
+nitesh=FullTimeEmployee(22000,2500,3700,2000)
+print("Pay to Nitesh:",nitesh.calculate_salary())
+print("Nitesh:")
+nitesh.get_details()
+
+rohit=PartTimeEmployee(8500,880)
+print("Pay to Rohit:",rohit.calculate_salary())
+print("Rohit:")
+rohit.get_details()
+
+
+print("Total Payroll:",nitesh.calculate_salary()+rohit.calculate_salary())
